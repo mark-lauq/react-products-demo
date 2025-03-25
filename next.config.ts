068d12d62a9685771d8655f7be5b942b6d67881f
Mark LauQ <mark.lauq@gmail.com>
2025-03-25 14:19:20 +0800

    feat(next): add API host environment variable and support for placeholder images

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  env: {
+    API_HOST: "https://api.escuelajs.co/api/v1",
+  },
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "i.imgur.com",
+      },
+      {
+        protocol: "https",
+        hostname: "via.placeholder.com",
       },
     ],
   },
